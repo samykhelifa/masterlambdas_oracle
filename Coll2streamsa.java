6682a7e39f459b6955ed59ed8b98f10c9200fe5f
samy <39716963+samykhelifa@users.noreply.github.com>
2019-08-30 23:05:10 +0200

    Update Coll2streamsa.java

--- Coll2streamsa.java
+++ Coll2streamsa.java
@@ -19,3 +19,14 @@ public class Coll2streamsa {
 
   }
 }
+/*  OUTPUT
+java.awt.Point[x=1,y=1]  :  1.4142135623730951
+java.awt.Point[x=2,y=2]  :  2.8284271247461903
+java.awt.Point[x=1,y=7]  :  7.0710678118654755
+java.awt.Point[x=0,y=3]  :  7.0710678118654755
+java.awt.Point[x=1,y=4]  :  7.0710678118654755
+java.awt.Point[x=2,y=5]  :  7.0710678118654755
+max distance   :  7.0710678118654755
+*/
+
+ 
