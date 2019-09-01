8cf09908a434168aa776da248445ca5b0c1703f1
samy <39716963+samykhelifa@users.noreply.github.com>
2019-09-01 18:35:20 +0200

    Create Seq2parallel.java

---
+++ Seq2parallel.java
@@ -0,0 +1,20 @@
+import java.util.OptionalDouble;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.Point;
+
+public class Seq2parallel {
+ public static void  main(String[] args){
+  
+    List<Integer> intList = Arrays.asList(1, 2,7, 3, 4, 5);
+ //   List<Point> pointList = new ArrayList<>(); 
+    OptionalDouble maxDistance = intList.parallelStream()
+       .map(i -> new Point(i % 3, i / 1)) 
+       .mapToDouble(p ->{ System.out.println(p +  " : " + p.distance(0,0));
+                          return  p.distance(0, 0);})
+       .max();
+       System.out.println( "max distance   :  " + maxDistance.getAsDouble());
+    
+    }
+}
